ffd1e2abc621d693dc6e98a1227366420d8078fb
Jens B <jens.bornschein@tu-dresden.de>
2018-11-30 13:20:10 +0100

    first stage for a szene graph renderer

---
+++ TangramLector/BrailleIO/Controller/SceneGraphDialogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tud.mci.tangram.TangramLector.BrailleIO.Controller
+{
+    class SceneGraphDialogController : IDisposable
+    {
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+    }
+}
